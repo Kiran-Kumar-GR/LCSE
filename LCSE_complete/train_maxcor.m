59aa6972ffd16dba7f53d36b314ac248ef4cf244
Kiran-Kumar-gr <57248399+Kiran-Kumar-gr@users.noreply.github.com>
2020-06-09 20:08:43 +0100

    Update train_maxcor.m

--- LCSE_complete/train_maxcor.m
+++ LCSE_complete/train_maxcor.m
@@ -1,10 +1,10 @@
-function model = train_maxcor(eeg, fs, num_fbs, n)
+function model = train_maxcor(data_train, fs, num_fbs, n)
 n_c=1:max(n);
-[num_chans, num_smpls, num_targs,num_trial] = size(eeg);
+[num_chans, num_smpls, num_targs,num_trial] = size(data_train);
 trains = zeros(num_targs, num_fbs, num_chans, num_smpls);
 W = zeros(num_fbs, num_targs, num_chans, (length(n_c)*num_trial));
 for i = 1:1:num_targs
-    eeg_tmp = squeeze(eeg(:, :,i,:));
+    eeg_tmp = squeeze(data_train(:, :,i,:));
     for j = 1:1:num_fbs
         eeg_tmp = filterbank(eeg_tmp, fs, j);
         trains(i,j,:,:) = squeeze(mean(eeg_tmp, 3));
@@ -16,13 +16,13 @@ model = struct('trains', trains, 'W', W,...
     'num_fbs', num_fbs, 'fs', fs, 'num_targs', num_targs);
 end
 
-function We = maxcor(eeg,n_c)
-[num_chans, num_smpls, num_trials]  = size(eeg);
+function We = maxcor(data_train,n_c)
+[num_chans, num_smpls, num_trials]  = size(data_train);
 S = zeros(num_smpls);
 We=[];
 % computing G
 for trial_i = 1:1:num_trials
-    x1 = squeeze(eeg(:,:,trial_i));
+    x1 = squeeze(data_train(:,:,trial_i));
     x1=reshape(zscore(x1(:),0,1),size(x1,1),size(x1,2));x1=x1';
     [~,Trans] = reduce_dimension(x1',num_chans-1);
     x=x1*Trans;
@@ -34,7 +34,7 @@ end
 [G1,~] = eigs(S);
 G =G1(:,n_c);
 for trial_i = 1:1:num_trials
-    x1 = squeeze(eeg(:,:,trial_i));
+    x1 = squeeze(data_train(:,:,trial_i));
     x1=reshape(zscore(x1(:),0,1),size(x1,1),size(x1,2));x1=x1';
     Q=real((((x1'*x1)^-1)*x1')*G);
     We=[We Q];
